5d8c6caadf678db8b32ef1a29fe53b42f413762b
Vũ Đại Minh <98660286+MinhVD1111@users.noreply.github.com>
2022-11-06 15:20:17 +0700

    Update KeyBoard.java
    W A S D

--- src/uet/oop/bomberman/Input/KeyBoard.java
+++ src/uet/oop/bomberman/Input/KeyBoard.java
@@ -5,7 +5,8 @@ import uet.oop.bomberman.Board;
 import uet.oop.bomberman.Graphics.Sprite;
 
 public class KeyBoard {
-    public boolean right, left, up, down, space, stop, start;
+    public boolean right, left, up, down,
+            space, stop, start,restart;
 
     public KeyBoard() {
         right = false;
@@ -15,44 +16,56 @@ public class KeyBoard {
         space = false;
         stop = false;
         start = true;
+        restart = false;
     }
 
     public void status(Scene gameScene) {
         gameScene.setOnKeyPressed(event -> {
             switch (event.getCode()) {
                 case LEFT:
+                case A:
                     left = true;
                     break;
                 case RIGHT:
+                case D:
                     right = true;
                     break;
                 case UP:
+                case W:
                     up = true;
                     break;
                 case DOWN:
+                case S:
                     down = true;
                     break;
                 case SPACE:
                     space = true;
                     break;
+
             }
         });
 
         gameScene.setOnKeyReleased(event -> {
             switch (event.getCode()) {
                 case LEFT:
+                case A:
                     left = false;
                     Board.getPlayer().setImg(Sprite.player_left.getFxImage());
                     break;
                 case RIGHT:
+                case D:
                     right = false;
                     Board.getPlayer().setImg(Sprite.player_right.getFxImage());
                     break;
                 case UP:
+                case W:
                     up = false;
+                    Board.getPlayer().setImg(Sprite.player_up.getFxImage());
                     break;
                 case DOWN:
+                case S:
                     down = false;
+                    Board.getPlayer().setImg(Sprite.player_down.getFxImage());
                     break;
                 case SPACE:
                     space = false;
@@ -62,8 +75,3 @@ public class KeyBoard {
     }
 
 }
-
-
-
-
-
